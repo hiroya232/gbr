6cd170d4b31e1979cbfba9985825cad274fa7ce0
hiroya-akeoka <mowoarm232@gmail.com>
2022-01-28 22:00:10 +0900

    :sparkles:ホストマシン上で起動中のChromiumでログイン方法選択画面へ遷移

---
+++ src/border.py
@@ -0,0 +1,11 @@
+import asyncio
+from pyppeteer import connect
+
+
+async def main():
+    browser = await connect(browserWSEndpoint='ws://host.docker.internal:9222/devtools/browser/6dbbf347-a599-473c-a8cc-24fdd1aa3aad')
+
+    pages = await browser.pages()
+    await pages[-1].goto('http://game.granbluefantasy.jp/#authentication', waitUntill=['domcontentloaded'])
+
+asyncio.get_event_loop().run_until_complete(main())
