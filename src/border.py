ebfdb19841ba8b744a7d337a4ec4c095d13889a1
hiroya-akeoka <mowoarm232@gmail.com>
2022-01-29 11:38:45 +0900

    :sparkles:ホストマシン上で起動中のChromiumでログイン

--- src/border.py
+++ src/border.py
@@ -1,5 +1,15 @@
 import asyncio
 from pyppeteer import connect
+import os
+from dotenv import load_dotenv
+
+
+async def login(pages):
+    load_dotenv('.env')
+    await pages.type('#subject-id', os.environ.get('MOBAGE_ID'))
+    await pages.type('#subject-password', os.environ.get('MOBAGE_PASSWORD'))
+    await pages.waitFor(1000)
+    await pages.click('#login')
 
 
 async def main():
@@ -10,5 +20,10 @@ async def main():
 
     await pages[-1].waitForSelector('#mobage-login')
     await pages[-1].click('#mobage-login')
+
+    await pages[-1].waitFor(1000)
+
+    pages = await browser.pages()
+    await login(pages[-1])
 
 asyncio.get_event_loop().run_until_complete(main())
